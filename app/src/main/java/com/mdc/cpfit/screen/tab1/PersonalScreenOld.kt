bc8a7598f805f79c4d7da16d93fa5e0e6cf65777
Chalermpong Dejnarong <d.chalermpong@gmail.com>
2019-01-12 14:05:20 +0700

    Update screen to new design

--- app/src/main/java/com/mdc/cpfit/screen/tab1/PersonalScreenOld.kt
+++ app/src/main/java/com/mdc/cpfit/screen/tab1/PersonalScreenOld.kt
@@ -1,11 +1,12 @@
 package com.mdc.cpfit.screen.tab1
 
-import android.graphics.Typeface
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.mdc.cpfit.R
@@ -16,19 +17,21 @@ import com.mdc.cpfit.screen.tab1.adapter.WalkingHistoryReportAdapter
 import com.mdc.cpfit.util.ImageUtil
 import com.mdc.cpfit.util.ScreenUnit
 import kotlinx.android.synthetic.main.sc_personal.*
+import retrofit2.Response
 
 
-class PersonalScreen : ScreenUnit() {
 
-    val TAG = PersonalScreen::class.java.simpleName
+class PersonalScreenOld : ScreenUnit() {
+
+    val TAG = PersonalScreenOld::class.java.simpleName
     var rootView: View? = null
     var type: String = ""
     lateinit var header: TextView
     lateinit var dialog: DialogBase
 
     companion object {
-        fun newInstance(): PersonalScreen {
-            val fragment = PersonalScreen()
+        fun newInstance(): PersonalScreenOld {
+            val fragment = PersonalScreenOld()
             val args = Bundle()
             fragment.setArguments(args)
             return fragment
@@ -37,13 +40,13 @@ class PersonalScreen : ScreenUnit() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setFrangment(PersonalScreen::class.simpleName.toString(), rootView)
+        setFrangment(PersonalScreenOld::class.simpleName.toString(), rootView)
         setValue()
 
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        rootView = inflater?.inflate(R.layout.sc_personal, container, false)
+        rootView = inflater?.inflate(R.layout.sc_personal_old, container, false)
         return rootView
     }
 
@@ -54,12 +57,30 @@ class PersonalScreen : ScreenUnit() {
 
     private fun setComponent() {
         //set profile
-        tvStepUnit.setTypeface(null, Typeface.BOLD)
-        tvKmUnit.setTypeface(null, Typeface.BOLD)
-
         Glide.with(this.context!!).load(R.drawable.ic_personal_profile)
                 .apply(ImageUtil.getImageCirclePersonnalProfile())
                 .into(imvProfile)
+
+        //set recycler view
+        var adapter = WalkingHistoryReportAdapter(activityMain)
+        recHistory.setLayoutManager(LinearLayoutManager(context))
+        recHistory.adapter = adapter
+
+        //Mockup
+        var mockData = ArrayList<WalkingHistoryReportBody>()
+        mockData.add(WalkingHistoryReportBody(1,1, 50.5F,30F))
+        mockData.add(WalkingHistoryReportBody(2,2, 51.5F,30.1F))
+        mockData.add(WalkingHistoryReportBody(3,3, 52.5F,30.2F))
+        mockData.add(WalkingHistoryReportBody(4,4, 53.5F,30F))
+        mockData.add(WalkingHistoryReportBody(5,5, 54.5F,30F))
+        mockData.add(WalkingHistoryReportBody(6,6, 50F,30F))
+        mockData.add(WalkingHistoryReportBody(7,7, 51.5F,30.5F))
+        mockData.add(WalkingHistoryReportBody(8,8, 52.5F,30F))
+        mockData.add(WalkingHistoryReportBody(9,9, 53.5F,30.2F))
+        mockData.add(WalkingHistoryReportBody(10,10, 54.5F,30F))
+        mockData.add(WalkingHistoryReportBody(11,11, 55.5F,30F))
+        adapter.updateArray(mockData)
+
 
         viewStepBtn.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
