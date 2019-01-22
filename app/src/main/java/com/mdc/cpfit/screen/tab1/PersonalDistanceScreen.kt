c393d665141e527eee8968a6535845fb04456219
Chalermpong Dejnarong <d.chalermpong@gmail.com>
2019-01-22 23:42:12 +0700

    Add animation

--- app/src/main/java/com/mdc/cpfit/screen/tab1/PersonalDistanceScreen.kt
+++ app/src/main/java/com/mdc/cpfit/screen/tab1/PersonalDistanceScreen.kt
@@ -1,11 +1,18 @@
 package com.mdc.cpfit.screen.tab1
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.TimeInterpolator
 import android.app.FragmentTransaction
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.OvershootInterpolator
 import android.widget.TextView
 import com.mdc.cpfit.R
 import com.mdc.cpfit.dialog.DialogBase
@@ -13,12 +20,12 @@ import com.mdc.cpfit.util.ScreenUnit
 import kotlinx.android.synthetic.main.partial_personal_distance.*
 
 
-class PersonalDistanceScreen: ScreenUnit() {
+class PersonalDistanceScreen : ScreenUnit() {
 
     val TAG = PersonalDistanceScreen::class.java.simpleName
     var rootView: View? = null
     var type: String = ""
-    var showOrHiddenCallBack:(()-> Unit)? = null
+    var showOrHiddenCallBack: (() -> Unit)? = null
 
 
     lateinit var dialog: DialogBase
@@ -41,6 +48,7 @@ class PersonalDistanceScreen: ScreenUnit() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setFrangment(PersonalDistanceScreen::class.simpleName.toString(), rootView)
+        setAnimation()
         setValue()
 
     }
@@ -72,15 +80,57 @@ class PersonalDistanceScreen: ScreenUnit() {
 //        tvTotalRecordTotalDistance.setTypeface(null, Typeface.BOLD)
 
 
-
         imvAdd.setOnClickListener {
             showOrHiddenCallBack?.invoke()
         }
 
     }
 
+//
+//    /**
+//     * rotation, alpha, scaleX/Y, rotationX/Y, translationX/Y
+//      */
+//    private fun setAnimation(view: View, animationName: String, timeInterpolar: TimeInterpolator, fromValue: Float = 0.1F, toValue: Float = 0.1F ,duration: Long, delay: Long = 0, animatorListener: Animator.AnimatorListener, reverse : Boolean = false) {
+////       var anim = ObjectAnimator()
+////        anim.setTarget(view)
+////        anim.setProperty(View.ALPHA)
+////        anim.setFloatValues(0.1F)
+//        var anim = ObjectAnimator.ofFloat(view, animationName, fromValue, toValue)
+//        anim.setDuration(duration)
+//        anim.setStartDelay(delay)
+//        anim.setInterpolator(timeInterpolar)
+//        if (reverse) anim.reverse()
+//        anim.addListener(animatorListener)
+////        anim.start()
+//
+//        var animSet = AnimatorSet()
+//        animSet.duration = 1000
+//        animSet.playTogether(anim,anim)
+//        anim.start()
+//    }
 
+    private fun setAnimation(reverse: Boolean = false) {
+        var animItem1 = ObjectAnimator.ofFloat(cardViewItem1, "translationX", 1000F, 0F)
+        var animItem2 = ObjectAnimator.ofFloat(cardViewItem2, "translationX", 1000F, 0F)
+        var animItem3 = ObjectAnimator.ofFloat(cardViewItem3, "translationX", 1000F, 0F)
 
+        animItem1.startDelay = 0
+        animItem2.startDelay = 90
+        animItem3.startDelay = 180
+
+        if(reverse) {
+            animItem1.repeatMode = ObjectAnimator.REVERSE
+            animItem2.repeatMode = ObjectAnimator.REVERSE
+            animItem3.repeatMode = ObjectAnimator.REVERSE
+        }
+
+        var animSet = AnimatorSet()
+        animSet.setDuration(800)
+        animSet.setInterpolator(OvershootInterpolator())
+        animSet.playTogether(animItem1, animItem2, animItem3)
+        animSet.start()
+
+    }
 
 
 }
